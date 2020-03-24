abbbb47424f413d53e81e946f50cc70235c05f01
Nikolaos Alexiou <nalexiou@users.noreply.github.com>
2020-03-24 13:19:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) =>{
-	res.send('Hello Node.js v 2.0\n');
+	res.send('Hello Node.js v 2.0\n Hello from Nick!');
 });
 
 app.listen(PORT, HOST);
