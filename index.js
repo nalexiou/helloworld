d941a3812b0fc0d4a44651752251f1791707d34d
Nikolaos Alexiou <nalexiou@users.noreply.github.com>
2020-03-24 13:40:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) =>{
-	res.send('Hello Node.js v 2.0\n Hello from Nick!');
+	res.send('Hello Node.js v 2.0\n New image has been deployed!');
 });
 
 app.listen(PORT, HOST);
